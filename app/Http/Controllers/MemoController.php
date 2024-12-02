d4fb88aa4bacfb2bbb19a299bd4279b621f06935
yoshinori-kiyota <157315602+YoshinoriKiyota@users.noreply.github.com>
2024-12-02 15:22:22 +0900

    test

---
+++ app/Http/Controllers/MemoController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Memo;
+
+class MemoController extends Controller
+{
+    // home.blade.phpを表示
+    public function index()
+    {
+        $memos = Memo::all();
+        return view('home', compact('memos'));
+    }
+
+    // submit.blade.phpを表示
+    public function create($id = 0)
+    {
+        if ($id != 0) {
+            $memo = Memo::where('id', $id)->get()->first();
+        } else {
+            $memo = (object) ["id" => 0, "title" => "", "content" => ""];
+        }
+        return view('submit', compact('memo'));
+    }
+
+    public function store(Request $request, $id = 0)
+    {
+        $title = $request->input('title');
+        $content = $request->input('content');
+        if ($id == 0) {
+            Memo::create([
+                'title' => $title,
+                'content' => $content
+            ]);
+        } else {
+            $memo = Memo::find($id);
+            $memo->update([
+                'title' => $title,
+                'content' => $content
+            ]);
+        }
+        return redirect()->route('home');
+    }
+
+    public function destroy($id)
+    {
+        Memo::destroy($id);
+        return redirect()->route('home');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\MemoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [MemoController::class, 'index'])->name('home');
+
+Route::get('/submit/{id}', [MemoController::class, 'create'])->name('showSubmit');
+
+Route::post('/submit/{id}', [MemoController::class, 'store'])->name('postSubmit');
+
+Route::get('/delete/{id}', [MemoController::class, 'destroy'])->name('delete');
